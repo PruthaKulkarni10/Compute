2020e617a0fd389e253019b296709b2ff7f2274e
PruthaKulkarni10 <130536825+PruthaKulkarni10@users.noreply.github.com>
2023-09-01 13:44:05 +0530

    quest4.py

--- quest4.py
+++ quest4.py
@@ -1,6 +1,25 @@
-n=int(input())
-li=[]
+if __name__ == '__main__':
+    N = int(input())
+    lst = []
 
-for i in li:
-    if str(input(""))
-    
+    for _ in range(N):
+        command = input().split()
+
+        if command[0] == 'insert':
+            i = int(command[1])
+            e = int(command[2])
+            lst.insert(i, e)
+        elif command[0] == 'print':
+            print(lst)
+        elif command[0] == 'remove':
+            e = int(command[1])
+            lst.remove(e)
+        elif command[0] == 'append':
+            e = int(command[1])
+            lst.append(e)
+        elif command[0] == 'sort':
+            lst.sort()
+        elif command[0] == 'pop':
+            lst.pop()
+        elif command[0] == 'reverse':
+            lst.reverse()
